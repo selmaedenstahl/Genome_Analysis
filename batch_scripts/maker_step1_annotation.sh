cbb351ce97d02395bee5a35e8bcd192387da3925
Selma Edenståhl <selmaedenstahl@student-236-214.eduroam.uu.se>
2019-05-07 15:30:39 +0200

    Added script for running maker step1

---
+++ batch_scripts/maker_step1_annotation.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -l
+
+#SBATCH -A g2019003
+#SBATCH -p core
+#SBATCH -n 4
+#SBATCH -t 20:00:00
+#SBATCH -J maker_annotation_selma_edenstahl
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user selma.edenstahl@gmail.com
+
+# Load modules
+module load bioinfo-tools
+module load maker/3.01.1-beta
+
+# My commands
+cd /home/seed5004/genome_analyses/analyses/04_annotation/maker_annotation/
+
+maker
