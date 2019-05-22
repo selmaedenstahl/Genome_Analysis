54712fe622a4de6a2668413d9bc086d14141ad09
Selma Edenståhl <selmaedenstahl@Selmas-MBP.lan>
2019-05-22 14:58:43 +0200

    Changed paths

--- batch_scripts/augustus_optimization.sh
+++ batch_scripts/augustus_optimization.sh
@@ -3,18 +3,17 @@
 #SBATCH -A g2019003
 #SBATCH -p core
 #SBATCH -n 4
-#SBATCH -t 20:00:00
+#SBATCH -t 10:00:00
 #SBATCH -J augustus_opt_selma_edenstahl
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user selma.edenstahl@gmail.com
 
 # Load modules
 module load bioinfo-tools
-module load maker/3.01.1-beta
-module load augustus
+module load maker/2.31.9
 
 # My commands
 
-cd /home/seed5004/genome_analyses/analyses/04_annotation/maker_annotation/step_1/pilon_assembly_correction_scaffold_11.maker.output
+cd /home/seed5004/genome_analyses/analyses/04_annotation/maker_annotation/scaffold-11_from_paper.maker.output
 
 optimize_augustus.pl --species=arabidopsis augustus.gbk.train --AUGUSTUS_CONFIG_PATH=config
