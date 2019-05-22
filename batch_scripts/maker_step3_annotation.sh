68e3d0f6fdc3a4603bd6fbc7274ef11e01807eb1
Selma Edenståhl <selmaedenstahl@student-238-45.eduroam.uu.se>
2019-05-22 10:25:17 +0200

    Added changes to running path

--- batch_scripts/maker_step3_annotation.sh
+++ batch_scripts/maker_step3_annotation.sh
@@ -3,18 +3,18 @@
 #SBATCH -A g2019003
 #SBATCH -p core
 #SBATCH -n 4
-#SBATCH -t 10:00:00
+#SBATCH -t 15:00:00
 #SBATCH -J maker_annotation_step3_selma_edenstahl
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user selma.edenstahl@gmail.com
 
 # Load modules
 module load bioinfo-tools
-module load maker/3.01.1-beta
+module load maker
 
 # My commands
 
-cd /home/seed5004/genome_analyses/analyses/04_annotation/maker_annotation/step_1/
+cd /home/seed5004/genome_analyses/analyses/04_annotation/maker_annotation/
 
 maker
 
