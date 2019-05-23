bebf66e59d7f63be4561bde0048e9cd874f802d3
Selma Edenståhl <selmaedenstahl@Selmas-MacBook-Pro.local>
2019-05-23 12:03:01 +0200

    New path

--- batch_scripts/augustus_optimization.sh
+++ batch_scripts/augustus_optimization.sh
@@ -3,17 +3,17 @@
 #SBATCH -A g2019003
 #SBATCH -p core
 #SBATCH -n 4
-#SBATCH -t 10:00:00
+#SBATCH -t 15:00:00
 #SBATCH -J augustus_opt_selma_edenstahl
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user selma.edenstahl@gmail.com
 
 # Load modules
 module load bioinfo-tools
-module load maker/2.31.9
+module load maker
 
 # My commands
 
 cd /home/seed5004/genome_analyses/analyses/04_annotation/maker_annotation/scaffold-11_from_paper.maker.output
 
-optimize_augustus.pl --species=arabidopsis augustus.gbk.train --AUGUSTUS_CONFIG_PATH=config
+optimize_augustus.pl --species=arabidopsis augustus.gbk.train --cpus=4 --AUGUSTUS_CONFIG_PATH=/home/seed5004/genome_analyses/analyses/04_annotation/maker_annotation/scaffold-11_from_paper.maker.output/config/
