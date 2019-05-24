da5b0f72c6555c269c57789b9cbc635a892d1517
Selma Edenståhl <selmaedenstahl@student-208-180.eduroam.uu.se>
2019-05-24 15:41:53 +0200

    Added changes to flags

--- batch_scripts/htseq_count.sh
+++ batch_scripts/htseq_count.sh
@@ -1,5 +1,5 @@
 for file in /home/seed5004/genome_analyses/analyses/03_transcriptome_assembly/tophat_alignment/tophat_output_sorted/*
 do
 	output=$(basename $file)
-	htseq-count -f bam -s no -t gene -i ID -r pos $file /home/seed5004/genome_analyses/data/ncbi_data/annotation_scaffold-11.gff3 -o /home/seed5004/genome_analyses/analyses/04_annotation/htseq_count/htseq_output_$output
+	htseq-count -f bam -t CDS -i Name -r pos $file /home/seed5004/genome_analyses/data/ncbi_data/annotation_scaffold-11.gff3 -o /home/seed5004/genome_analyses/analyses/04_annotation/htseq_count/htseq_output_$output.txt
 done
